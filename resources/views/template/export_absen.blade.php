72a2963accc629690c9e59cdfa3a9acb236aae2f
!Github@251423! <studiocode52@gmail.com>
2024-05-30 17:15:37 +0700

    debug

--- resources/views/template/export_absen.blade.php
+++ resources/views/template/export_absen.blade.php
@@ -57,7 +57,7 @@
                                                 $dataDetail = $data[$tanggal];
                                                 foreach($dataDetail as $detail){
                                                     $info = $detail->info;
-                                                    dd($info);
+                                                    dd($info['type']);
                                                     switch ($info->type) {
                                                         case 'absen':
                                                             if (empty($info?->keterangan?->masuk) && empty($info?->keterangan?->keluar)) {
