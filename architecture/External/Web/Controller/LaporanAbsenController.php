bc83fa788dbb00430fa261038709028bc92a337a
!Github@251423! <studiocode52@gmail.com>
2024-05-31 08:51:02 +0700

    modified:   architecture/External/Web/Controller/LaporanAbsenController.php

--- architecture/External/Web/Controller/LaporanAbsenController.php
+++ architecture/External/Web/Controller/LaporanAbsenController.php
@@ -67,6 +67,7 @@ class LaporanAbsenController extends Controller
             }
             $laporan = $this->queryBus->ask(new GetAllLaporanAbsenQuery($nidn,$nip,$tanggal_mulai,$tanggal_akhir,TypeData::Default));
 
+            dd($this->generateHtml(true, 0, null, null, $laporan));
             if($type_export=="pdf"){
                 $file = PdfX::From(
                     "template.export_absen", 
@@ -86,5 +87,170 @@ class LaporanAbsenController extends Controller
             Session::flash(TypeNotif::Error->val(), $e->getMessage());
             return redirect()->route('izin.index');
         }
+    }
+
+    public function generateHtml($initial=true,$index=0,$i_t=null,$i_data=null,$source)
+    {
+        $html = "";
+        if($initial){
+            $html .= '<div class="card-body row">';
+            $html .= '<div class="col-12">';
+            $html .= '<h5>Per Tanggal ' . $source['start'] . ' - ' . $source['end'] . '</h5>';
+            $html .= '</div>';
+            $html .= '<div class="col-12 table-responsive">';
+            $html .= '<table id="tb" class="table table-striped text-center">';
+            $html .= '<thead>';
+            $html .= '<tr>';
+            $html .= '<th>#</th>';
+            $html .= '<th>Nama</th>';
+
+            foreach ($source['list_tanggal'] as $tanggal) {
+                $html .= '<th>' . Carbon::parse($tanggal)->setTimezone('Asia/Jakarta')->format('d') . '</th>';
+            }
+            
+            $html .= '</tr>';
+            $html .= '</thead>';
+            $html .= '<tbody>';
+        }
+
+        if(!$initial && !is_null($i_data)){
+            $data = $source['list_data'][$i_data];
+            $nama = $data['type'] == "pegawai" ? $data['pengguna']['nama'] : $data['pengguna']['nama_dosen'];
+            $kode = $data['type'] == "pegawai" ? $data['pengguna']['nip'] : $data['pengguna']['NIDN'];
+
+            $html .= '<tr>';
+            $html .= '  <td>' . ($index + 1) . '</td>';
+            $html .= '  <td>' . $nama . ' - ' . $kode . '</td>';
+
+            $tanggal = $source['list_tanggal'][$i_t];
+
+            $html .= '  <td>';
+            $html .= '      <table>';
+            $html .= '          <tr>';
+            $html .= '              <td class="column_min">';
+
+            $aturan_jam = "08:00 - 15:00";
+            $dayOfWeek = Carbon::parse($tanggal)->setTimezone('Asia/Jakarta')->dayOfWeek;
+            if ($dayOfWeek == Carbon::FRIDAY) {
+                $aturan_jam = "08:00 - 14:00";
+            } elseif ($dayOfWeek == Carbon::SATURDAY) {
+                $aturan_jam = "08:00 - 12:00";
+            }
+
+            $html .= $aturan_jam;
+            $html .= '              </td>';
+            $html .= '          </tr>';
+
+            $keterangan = "";
+            if (isset($data[$tanggal])) {
+                foreach ($data[$tanggal] as $detail) {
+                    $info = $detail['info'];
+                    switch ($info->type) {
+                        case 'absen':
+                            if (empty($info->keterangan['masuk']) && empty($info->keterangan['keluar'])) {
+                                $keterangan = "<span class='badge bg-danger'>Tidak Masuk</span>";
+                            } elseif (!empty($info->keterangan['masuk']) && empty($info->keterangan['keluar'])) {
+                                $masuk = Carbon::parse($info->keterangan['masuk'])->setTimezone('Asia/Jakarta')->format('H:i');
+                                $keterangan = "<span class='badge bg-success'>" . $masuk . "</span> - <span class='badge bg-danger'>Masih Masuk</span>";
+                            } else {
+                                $masuk = Carbon::parse($info->keterangan['masuk'])->setTimezone('Asia/Jakarta')->format('H:i');
+                                $keluar = Carbon::parse($info->keterangan['keluar'])->setTimezone('Asia/Jakarta')->format('H:i');
+                                $keterangan = "<span class='badge bg-success'>" . $masuk . "</span> - <span class='badge bg-danger'>" . $keluar . "</span>";
+                            }
+                            break;
+                        case 'izin':
+                            $keterangan = "<span class='badge bg-primary'>Izin</span>";
+                            break;
+                        case 'cuti':
+                            $keterangan = "<span class='badge bg-warning text-black'>Cuti</span>";
+                            break;
+                    }
+                }
+            }
+            $html .= '          <tr>';
+            $html .= '              <td>' . $keterangan . '</td>';
+            $html .= '          </tr>';
+            $html .= '      </table>';
+            $html .= '  </td>';
+            $html .= '</tr>';
+
+            if($i_t<count($source['list_data'])){
+                $html .= $this->generateHtml(false, $index, $i_t+1, $i_data, $source);
+            }
+        }
+
+        if($i_data<count($source['list_tanggal'])){
+            $html .= $this->generateHtml(false, $index, $i_t, $i_data+1, $source);
+        }
+
+        // foreach ($source['list_data'] as $index => $data) {
+        //     // $nama = $data['type'] == "pegawai" ? $data['pengguna']['nama'] : $data['pengguna']['nama_dosen'];
+        //     // $kode = $data['type'] == "pegawai" ? $data['pengguna']['nip'] : $data['pengguna']['NIDN'];
+
+        //     // $html .= '<tr>';
+        //     // $html .= '<td>' . ($index + 1) . '</td>';
+        //     // $html .= '<td>' . $nama . ' - ' . $kode . '</td>';
+
+        //     foreach ($source['list_tanggal'] as $tanggal) {
+        //         // $html .= '<td>';
+        //         // $html .= '<table>';
+        //         // $html .= '<tr>';
+        //         // $html .= '<td class="column_min">';
+
+        //         // $aturan_jam = "08:00 - 15:00";
+        //         // $dayOfWeek = Carbon::parse($tanggal)->setTimezone('Asia/Jakarta')->dayOfWeek;
+        //         // if ($dayOfWeek == Carbon::FRIDAY) {
+        //         //     $aturan_jam = "08:00 - 14:00";
+        //         // } elseif ($dayOfWeek == Carbon::SATURDAY) {
+        //         //     $aturan_jam = "08:00 - 12:00";
+        //         // }
+
+        //         // $html .= $aturan_jam;
+        //         // $html .= '</td>';
+        //         // $html .= '</tr>';
+
+        //         // $keterangan = "";
+        //         if (isset($data[$tanggal])) {
+        //             // foreach ($data[$tanggal] as $detail) {
+        //             //     $info = $detail['info'];
+        //             //     switch ($info->type) {
+        //             //         case 'absen':
+        //             //             if (empty($info->keterangan['masuk']) && empty($info->keterangan['keluar'])) {
+        //             //                 $keterangan = "<span class='badge bg-danger'>Tidak Masuk</span>";
+        //             //             } elseif (!empty($info->keterangan['masuk']) && empty($info->keterangan['keluar'])) {
+        //             //                 $masuk = Carbon::parse($info->keterangan['masuk'])->setTimezone('Asia/Jakarta')->format('H:i');
+        //             //                 $keterangan = "<span class='badge bg-success'>" . $masuk . "</span> - <span class='badge bg-danger'>Masih Masuk</span>";
+        //             //             } else {
+        //             //                 $masuk = Carbon::parse($info->keterangan['masuk'])->setTimezone('Asia/Jakarta')->format('H:i');
+        //             //                 $keluar = Carbon::parse($info->keterangan['keluar'])->setTimezone('Asia/Jakarta')->format('H:i');
+        //             //                 $keterangan = "<span class='badge bg-success'>" . $masuk . "</span> - <span class='badge bg-danger'>" . $keluar . "</span>";
+        //             //             }
+        //             //             break;
+        //             //         case 'izin':
+        //             //             $keterangan = "<span class='badge bg-primary'>Izin</span>";
+        //             //             break;
+        //             //         case 'cuti':
+        //             //             $keterangan = "<span class='badge bg-warning text-black'>Cuti</span>";
+        //             //             break;
+        //             //     }
+        //             // }
+        //         }
+
+        //         // $html .= '<tr>';
+        //         // $html .= '<td>' . $keterangan . '</td>';
+        //         // $html .= '</tr>';
+        //         // $html .= '</table>';
+        //         // $html .= '</td>';
+        //     }
+
+        //     // $html .= '</tr>';
+        // }
+
+        // $html .= '</tbody>';
+        // $html .= '</table>';
+        // $html .= '</div>';
+        // $html .= '</div>';
+
+        return $html;
     }
 }
