29b16ab6be2b2c4ab3604260af1cca9fa8a65ee3
!Github@251423! <studiocode52@gmail.com>
2024-06-05 14:11:56 +0700

    bug

--- architecture/External/Web/Controller/LaporanAbsenController.php
+++ architecture/External/Web/Controller/LaporanAbsenController.php
@@ -129,7 +129,7 @@ class LaporanAbsenController extends Controller
         } catch (Exception $e) {
             throw $e;
             Session::flash(TypeNotif::Error->val(), $e->getMessage());
-            return redirect()->route('izin.index');
+            return redirect()->route('laporan_absen.index');
         }
     }
 
