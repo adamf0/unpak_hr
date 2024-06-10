29e759b3e8a68b85e07d307967758ce2cbdffbdd
!Github@251423! <studiocode52@gmail.com>
2024-06-10 17:10:47 +0700

    auth design

--- resources/views/authorization.blade.php
+++ resources/views/authorization.blade.php
@@ -21,65 +21,63 @@
   }
 </style>
 <body>
-  <main>
-    <div class="container">
-
-      <section class="section register min-vh-100 flex-column align-items-center justify-content-center py-4">
+      <section class="bg-light p-3 p-md-4 p-xl-5">
         <div class="container">
           <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 flex-column align-items-center justify-content-center text-center">
-
-              <div class="flex-row align-items-center justify-content-center py-4">
-                <a href="#" class="logo text-decoration-none align-items-center">
-                  <img src="{{ Utility::loadAsset('assets/img/logo.webp') }}" alt="">
-                  <span class="d-none d-lg-block">HR Portal</span>
-                </a>
-                <!-- <small class="d-none d-lg-block">Sistem Informasi Penelitian, Abdimas dan PubliKaSI</small> -->
-              </div><!-- End Logo -->
-
-              <div class="card mb-3">
-
-                <div class="card-body">
-
-                  <div>
-                    {{ Utility::showNotif() }}
-                    <h5 class="card-title text-center pb-0 fs-4">Masuk ke akun Anda</h5>
-                    <p class="text-center small">Masukkan username & password Anda untuk masuk</p>
+            <div class="col-12 col-xxl-11">
+              <div class="card border-light-subtle shadow-sm">
+                <div class="row g-0">
+                  <div class="col-12 col-md-6">
+                    <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="https://bootstrapbrain.com/demo/components/logins/login-4/assets/img/logo-img-1.webp" alt="background">
                   </div>
-                  
-                  <form class="row g-3 needs-validation" action="{{route('auth.authentication')}}" method="post">
-                    @csrf
-                    <div class="col-12">
-                      <label for="yourUsername" class="form-label">NIDN/Username</label>
-                      <div class="input-group has-validation">
-                        <!-- <span class="input-group-text" id="inputGroupPrepend">@</span> -->
-                        <input type="text" name="username" class="form-control" id="yourUsername" required>
-                        <div class="invalid-feedback">Silakan Masukkan NIDN / Nama Pengguna Anda.</div>
+                  <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                    <div class="col-12 col-lg-11 col-xl-10">
+                      <div class="card-body p-3 p-md-4 p-xl-5">
+                        <div class="row">
+                          <div class="col-12">
+                            <div class="mb-5">
+                              <div class="text-center mb-4">
+                                <a href="#!">
+                                  <img src="{{ Utility::loadAsset('assets/img/logo.webp') }}" alt="logo" width="100" height="100">
+                                </a>
+                              </div>
+                              <h4 class="text-center">Selamat Datang Di HR Portal</h4>
+                            </div>
+                          </div>
+                        </div>
+                        {{ Utility::showNotif() }}
+                        <form action="{{route('auth.authentication')}}" method="post">
+                          @csrf
+                          <div class="row gy-3 overflow-hidden">
+                            <div class="col-12">
+                              <div class="form-floating mb-3">
+                                <input type="text" name="username" class="form-control" id="yourUsername" required>
+                                <label for="username" class="form-label">Username</label>
+                              </div>
+                            </div>
+                            <div class="col-12">
+                              <div class="form-floating mb-3">
+                                <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password" required>
+                                <label for="password" class="form-label">Password</label>
+                              </div>
+                            </div>
+                            <div class="col-12">
+                              <div class="d-grid">
+                                <button class="btn btn-dark btn-lg" type="submit">Masuk</button>
+                              </div>
+                            </div>
+                          </div>
+                        </form>
                       </div>
                     </div>
-
-                    <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="yourPassword" required>
-                      <div class="invalid-feedback">Silahkan Masukkan Kata Sandi Anda!</div>
-                    </div>
-
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
-                    </div>
-                  </form>
-
+                  </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-
       </section>
 
-    </div>
-  </main><!-- End #main -->
   <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
