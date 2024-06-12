73e371e1bde528069d7f763909391d4bbdb352cc
!Github@251423! <studiocode52@gmail.com>
2024-06-12 13:18:23 +0700

    debug

--- architecture/External/Select2/Controller/Select2PresensiController.php
+++ architecture/External/Select2/Controller/Select2PresensiController.php
@@ -98,7 +98,8 @@ class Select2PresensiController extends Controller
         }, []);
 
         $list = $listPresensi->filter(function ($item) use ($listTanggal, $rangeTanggal) {
-            return !in_array($item->GetTanggal(), $listTanggal) && in_array($item->GetTanggal(),$rangeTanggal);
+            dd($item->GetTanggal(), $listTanggal, $item->GetAbsenMasuk() instanceof Date, $item->GetTanggal(),$rangeTanggal);
+            return !in_array($item->GetTanggal(), $listTanggal) && $item->GetAbsenMasuk() instanceof Date && in_array($item->GetTanggal(),$rangeTanggal);
         })
         ->values()
         ->sortBy('tanggal', SORT_REGULAR, true)
