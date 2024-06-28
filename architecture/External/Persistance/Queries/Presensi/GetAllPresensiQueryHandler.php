3f2f286269f9ca3b08c4d4d1b90166a57c5cc702
!Github@251423! <studiocode52@gmail.com>
2024-06-28 15:28:24 +0700

    debug

--- architecture/External/Persistance/Queries/Presensi/GetAllPresensiQueryHandler.php
+++ architecture/External/Persistance/Queries/Presensi/GetAllPresensiQueryHandler.php
@@ -21,46 +21,85 @@ class GetAllPresensiQueryHandler extends Query
     public function __construct() {}
 
     function getDosen($source){
-        return !is_null($source->Dosen)? Creator::buildDosen(DosenEntitas::make(
-            $source->Dosen?->NIDN,
-            $source->Dosen?->nama_dosen,
-            !is_null($source->Dosen->Fakultas)? Creator::buildFakultas(FakultasEntitas::make(
-                $source->Dosen?->Fakultas?->kode_fakultas,
-                $source->Dosen?->Fakultas?->nama_fakultas,
+        return is_null($source->nip_pegawai)? Creator::buildDosen(DosenEntitas::make(
+            $source->nidn_dosen,
+            $source->nama_dosen,
+            !is_null($source->kode_fakultas)? Creator::buildFakultas(FakultasEntitas::make(
+                $source->kode_fakultas,
+                $source->nama_fakultas,
             )):null,
-            !is_null($source->Prodi)? Creator::buildProdi(ProdiEntitas::make(
-                $source->Prodi?->kode_prodi,
-                $source->Prodi?->nama_prodi,
+            !is_null($source->kode_prodi)? Creator::buildProdi(ProdiEntitas::make(
+                $source->kode_prodi,
+                $source->nama_prodi,
             )):null,
+            // $source->status
         )):null;
     }
     function getPegawai($source){
-        return !is_null($source->Pegawai)? Creator::buildPegawai(PegawaiEntitas::make(
+        return !is_null($source->nip_pegawai)? Creator::buildPegawai(PegawaiEntitas::make(
             null,
-            $source->Pegawai?->nip,
-            $source->Pegawai?->nama,
-            $source->Pegawai?->unit,
+            $source->nip_pegawai,
+            $source->nama_pegawai,
+            $source->unit_kerja,
+            // $source->status
         )):null;
     }
 
     public function handle(GetAllPresensiQuery $query)
     {
-        $datas = ModelAbsensi::with([
-            'Dosen',
-            'Dosen.Fakultas',
-            'Dosen.Prodi',
-            'Pegawai'
-        ]);
+        $datas = DB::table('absen as a')
+        ->leftJoin('connect_n_pribadi as cnp', 'a.nip', '=', 'cnp.nip')
+        ->leftJoin('connect_m_dosen as cmd', 'a.nidn', '=', 'cmd.NIDN')
+        ->leftJoin('connect_e_pribadi as cep', 'cmd.NIDN', '=', 'cep.nidn')
+        ->leftJoin('connect_m_fakultas as cmf', 'cmd.kode_fak', '=', 'cmf.kode_fakultas')
+        ->leftJoin('connect_r_prodi as crp', 'cmd.kode_prodi', '=', 'crp.nama_prodi')
+        ->rightJoinSub(
+            DB::table('connect_n_pengangkatan')->select('nip', 'unit_kerja')
+                ->whereRaw('LOWER(status_n_pengangkatan) = ?', ['berlaku'])
+                ->unionAll(
+                    DB::table('connect_e_pengangkatan')->select('nip', 'unit_kerja')
+                        ->whereRaw('LOWER(status_berlaku_pengangkatan) = ?', ['berlaku'])
+                ),
+            'mu',
+            function ($join) {
+                $join->join('connect_payroll_m_pegawai as cpmp', 'mu.nip', '=', 'cpmp.nip')
+                    ->whereNull('cpmp.tgl_keluar')
+                    ->orWhereRaw('TRIM(cpmp.tgl_keluar) = ?', ['']);
+            }
+        )->rightJoin('connect_n_pribadi as ms', 'ms.nip', '=', 'cnp.nip')
+        ->select(
+            'cnp.nip as nip_pegawai',
+            'cnp.nama as nama_pegawai',
+            'cep.nip as nip_dosen',
+            'a.nidn as nidn_dosen',
+            'cmd.nama_dosen',
+            'cmf.*',
+            'crp.*',
+            'ms.unit_kerja',
+            'ms.status',
+            // DB::raw("(CASE 
+            //     WHEN cpmp.tgl_keluar IS NULL OR TRIM(cpmp.tgl_keluar) = '' THEN 'aktif'
+            //     ELSE 'keluar'
+            // END) as status"),
+            'a.*'
+        );
+        
+        // ModelAbsensi::with([
+        //     'Dosen',
+        //     'Dosen.Fakultas',
+        //     'Dosen.Prodi',
+        //     'Pegawai'
+        // ]);
         if(!empty($query->GetNIDN())){
-            $datas = $datas->where('nidn',$query->GetNIDN());
+            $datas = $datas->where('a.nidn',$query->GetNIDN());
         }
         if(!empty($query->GetNIP())){
-            $datas = $datas->where('nip',$query->GetNIP());
+            $datas = $datas->where('cnp.nip',$query->GetNIP())->orWhere('cep.nip',$query->GetNIP());
         }
         if(!empty($query->GetTahun())){
-            $datas = $datas->where(DB::raw('YEAR(tanggal)'),$query->GetTahun());
+            $datas = $datas->where(DB::raw('YEAR(a.tanggal)'),$query->GetTahun());
         }
-        $datas = $datas->orderBy('absen_masuk','DESC')->get();
+        $datas = $datas->orderBy('a.absen_masuk','DESC')->get();
 
         if($query->getOption()==TypeData::Default) return new Collection($datas);
 
