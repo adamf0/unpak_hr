467ba4815de0e6f0a5ed92fe85931365db5cce8a
!Github@251423! <studiocode52@gmail.com>
2024-07-02 12:21:07 +0700

    debug

--- resources/views/template/export_sppd.blade.php
+++ resources/views/template/export_sppd.blade.php
@@ -45,7 +45,7 @@
         width: 100%;
 
         .paraf__container{
-            flex-grow: 1;
+            /* flex-grow: 1; */
             width: 30%;
             /* text-align: center; */
         }
@@ -126,18 +126,18 @@
         </table>
         <div class="paraf">
             <div class="paraf__container">
-            <p>Menyetujui</p>
-            @if (!empty($sppd->EPribadiRemote) && $sppd->status=="terima sdm")
-            <img src='data:image/png;base64, {!! base64_encode(QrCode::size(100)->generate("Nama : ".$sppd->EPribadiRemote->nama)) !!}' alt="tanda tangan"/>
-            <br>
-            @else
-            <br>
-            <br>
-            @endif
-            <b>
-                {{$sppd->EPribadiRemote?->nama??"Tidak diketahui"}}<br>
-                {{$sppd->EPribadiRemote?->nip??"Tidak diketahui"}}
-            </b>
+                <p>Menyetujui</p>
+                @if (!empty($sppd->EPribadiRemote) && $sppd->status=="terima sdm")
+                <img src='data:image/png;base64, {!! base64_encode(QrCode::size(100)->generate("Nama : ".$sppd->EPribadiRemote->nama)) !!}' alt="tanda tangan"/>
+                <br>
+                @else
+                <br>
+                <br>
+                @endif
+                <b>
+                    {{$sppd->EPribadiRemote?->nama??"Tidak diketahui"}}<br>
+                    {{$sppd->EPribadiRemote?->nip??"Tidak diketahui"}}
+                </b>
             </div>
         </div>
         <br>
