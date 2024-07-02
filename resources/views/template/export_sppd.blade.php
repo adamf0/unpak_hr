31a5f95a129c539c9fbc5e471ce8fc7050d5c8c4
!Github@251423! <studiocode52@gmail.com>
2024-07-02 13:27:11 +0700

    debug

--- resources/views/template/export_sppd.blade.php
+++ resources/views/template/export_sppd.blade.php
@@ -171,7 +171,7 @@
             @foreach ($sppd->AnggotaFlat as $key => $anggota)
                 @if ($total_anggota>1 && $key%2==0)
                     <tr style="border: 1px solid black !important">
-                        <td style="border: 1px solid black !important; width: 400px;">
+                        <td style="border: 1px solid black !important; width: 200px;">
                             <center>
                             <p>Tanggal {{$anggota->tanggal}}</p>
                             <br>
@@ -187,7 +187,7 @@
                             </center>     
                         </td>
                     @elseif($total_anggota>1 && $key%2==1)
-                        <td style="border: 1px solid black !important; width: 400px;">
+                        <td style="border: 1px solid black !important; width: 200px;">
                             <center>
                             <p>Tanggal {{$anggota->tanggal}}</p>
                             <br>
@@ -205,7 +205,7 @@
                     </tr>
                     @else
                     <tr style="border: 1px solid black !important">
-                        <td colspan="2" style="border: 1px solid black !important; width: 400px;">
+                        <td colspan="2" style="border: 1px solid black !important; width: 200px;">
                             <center>
                             <p>Tanggal {{$anggota->tanggal}}</p>
                             <br>
