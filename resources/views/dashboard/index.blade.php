a6d93206757492c903be6dea500bfcb7cbbfcdf3
!Github@251423! <studiocode52@gmail.com>
2024-07-05 10:14:30 +0700

    bug

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -289,7 +289,7 @@
                         <span class="badge bg-info rounded-pill">&nbsp;</span>&nbsp;<p>SPPD</p>
                     </div>
                     <div class="col">
-                        <span class="badge bg-success rounded-pill">&nbsp;</span>&nbsp;<p>Masuk/SPPD</p>
+                        <span class="badge bg-success rounded-pill">&nbsp;</span>&nbsp;<p>Masuk</p>
                     </div>
                     <div class="col">
                         <span class="badge bg-black rounded-pill">&nbsp;</span>&nbsp;<p>Telat Masuk</p>
