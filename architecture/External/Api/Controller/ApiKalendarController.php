9e59d40f2d51662b73100210ff946085f4016f4f
!Github@251423! <studiocode52@gmail.com>
2024-07-05 09:02:11 +0700

    debug

--- architecture/External/Api/Controller/ApiKalendarController.php
+++ architecture/External/Api/Controller/ApiKalendarController.php
@@ -199,9 +199,6 @@ class ApiKalendarController extends Controller //data cuti, izin, sppd, absen be
             $skip_tanggal = array_merge($list_libur_, $list_cuti_, $list_izin_, $list_sppd_);
             // dd($list_absen,$list_libur_, $list_cuti_, $list_izin_);
             $listAbsen = $list_absen->reduce(function ($carry, $item) use ($format, $list_klaim_absen,$skip_tanggal) {
-                if($item->tanggal=="2024-06-17"){
-                    dd($item->tanggal,$skip_tanggal, !in_array($item->tanggal,$skip_tanggal) , !Carbon::parse($item->tanggal)->isSunday());
-                }
                 if(!in_array($item->tanggal,$skip_tanggal) && !Carbon::parse($item->tanggal)->isSunday()){
                     if ($format == "full-calendar") {
                         $klaim = $list_klaim_absen->where('status', 'terima')->where('tanggal', $item->tanggal);
@@ -250,7 +247,7 @@ class ApiKalendarController extends Controller //data cuti, izin, sppd, absen be
 
                 return $carry;
             }, []);
-
+            dd($listAbsen, $listKalendar, $listCuti, $listIzin, $listSPPD);
             $list = array_merge($listAbsen, $listKalendar, $listCuti, $listIzin, $listSPPD);
 
             return response()->json([
