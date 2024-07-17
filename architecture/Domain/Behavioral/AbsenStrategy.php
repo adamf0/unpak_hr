16f36ffc2fe49dfdbafdda18592141f49e09f4b0
!Github@251423! <studiocode52@gmail.com>
2024-07-17 09:45:56 +0700

    debug

--- architecture/Domain/Behavioral/AbsenStrategy.php
+++ architecture/Domain/Behavioral/AbsenStrategy.php
@@ -5,20 +5,9 @@ use Carbon\Carbon;
 
 class AbsenStrategy implements IAbsenStrategy {
     public function getBackground($klaim, $dataAbsen, $tanggal, $now) {
-        $aturanPulang = "14:59:00";
-        if (Carbon::now()->setTimezone('Asia/Jakarta')->dayOfWeek == Carbon::FRIDAY) {
-            $aturanPulang = "13:59:00";
-        } elseif (Carbon::now()->setTimezone('Asia/Jakarta')->dayOfWeek == Carbon::SATURDAY) {
-            $aturanPulang = "11:59:00";
-        }
-
-        //$jam_masuk = is_null($dataAbsen?->absen_masuk) ? $klaim?->jam_masuk : $dataAbsen?->absen_masuk;
-        $jam_keluar = is_null($dataAbsen?->absen_keluar) ? $klaim?->jam_keluar : $dataAbsen?->absen_keluar;
-        //$pulangCepat = is_null($jam_keluar)? false:strtotime($dataAbsen?->tanggal." ".$jam_keluar)<=strtotime($dataAbsen?->tanggal." ".$aturanPulang);
-        
         $warna = match(true){
             !empty($klaim?->jam_masuk) || !empty($klaim?->jam_keluar) => "#198754",
-            !empty($dataAbsen?->catatan_pulang) || !Utility::is8Hour($dataAbsen->tanggal, $dataAbsen->absen_masuk, $dataAbsen->absen_keluar) => "#000",
+            !empty($dataAbsen?->catatan_pulang) || !Utility::is8Hour($dataAbsen->tanggal, $dataAbsen->absen_masuk, $dataAbsen->absen_keluar) => "#808080",
             Utility::isLate($dataAbsen?->absen_masuk, $dataAbsen?->tanggal) => "#000",
             default => "#198754"
         };
