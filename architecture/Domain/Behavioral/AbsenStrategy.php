abf55fb01a09abda825230739b29e5340b155c13
!Github@251423! <studiocode52@gmail.com>
2024-07-17 10:35:34 +0700

    debug

--- architecture/Domain/Behavioral/AbsenStrategy.php
+++ architecture/Domain/Behavioral/AbsenStrategy.php
@@ -40,18 +40,9 @@ class AbsenStrategy implements IAbsenStrategy {
     }
 
     public function getTitle($klaim, $dataAbsen, $tanggal, $now) {
-        $aturanPulang = "14:59:00";
-        if (Carbon::now()->setTimezone('Asia/Jakarta')->dayOfWeek == Carbon::FRIDAY) {
-            $aturanPulang = "13:59:00";
-        } elseif (Carbon::now()->setTimezone('Asia/Jakarta')->dayOfWeek == Carbon::SATURDAY) {
-            $aturanPulang = "11:59:00";
-        }
-
         $jam_masuk = is_null($dataAbsen?->absen_masuk) ? $klaim?->jam_masuk : $dataAbsen?->absen_masuk;
         $jam_keluar = is_null($dataAbsen?->absen_keluar) ? $klaim?->jam_keluar : $dataAbsen?->absen_keluar;
-        $pulangCepat = is_null($jam_keluar)? false:strtotime($dataAbsen?->tanggal." ".$jam_keluar)<=strtotime($dataAbsen?->tanggal." ".$aturanPulang);
-        // /dump($dataAbsen?->tanggal." ".$jam_keluar, $dataAbsen?->tanggal." ".$aturanPulang);
-
+        
         $label = "";
         if(!empty($jam_masuk) && !empty($jam_keluar)){
             if(!empty($klaim?->jam_masuk) || !empty($klaim?->jam_keluar)){
@@ -71,12 +62,12 @@ class AbsenStrategy implements IAbsenStrategy {
                 Utility::isLate($jam_masuk, $dataAbsen?->tanggal) &&
                 !Utility::is8Hour($dataAbsen->tanggal, $jam_masuk, $jam_keluar)
             ){
-                $label = "(pulang)";
+                $label = "(pulang1)";
             } else if( 
                 !Utility::isLate($jam_masuk, $dataAbsen?->tanggal) &&
                 !Utility::is8Hour($dataAbsen->tanggal, $jam_masuk, $jam_keluar)
             ){
-                $label = "(pulang)";
+                $label = "(pulang2)";
             }
         } else{
             $label = "";
