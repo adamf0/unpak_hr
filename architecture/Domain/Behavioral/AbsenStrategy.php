40f60e83571c8c03aa87e2c29ec4e50ab0680631
!Github@251423! <studiocode52@gmail.com>
2024-07-17 10:45:02 +0700

    debug

--- architecture/Domain/Behavioral/AbsenStrategy.php
+++ architecture/Domain/Behavioral/AbsenStrategy.php
@@ -62,7 +62,7 @@ class AbsenStrategy implements IAbsenStrategy {
                 Utility::isLate($jam_masuk, $dataAbsen?->tanggal) &&
                 !Utility::is8Hour($dataAbsen->tanggal, $jam_masuk, $jam_keluar)
             ){
-                $label = "(pulang1)"; //masalah
+                $label = "(pulang)";
             } else if( 
                 !Utility::isLate($jam_masuk, $dataAbsen?->tanggal) &&
                 !Utility::is8Hour($dataAbsen->tanggal, $jam_masuk, $jam_keluar)
