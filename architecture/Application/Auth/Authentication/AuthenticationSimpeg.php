0703cfa5207c410e32ac9ffa72d44acb8879f123
!Github@251423! <studiocode52@gmail.com>
2024-08-01 09:13:31 +0700

    debug

--- architecture/Application/Auth/Authentication/AuthenticationSimpeg.php
+++ architecture/Application/Auth/Authentication/AuthenticationSimpeg.php
@@ -21,25 +21,26 @@ class AuthenticationSimpeg implements IAuthentication {
                                     'NPribadi'=>fn($query)=>$query->select('id_n_pribadi','nip','nama','status_pegawai')]
                                 )
                                 ->where('username',$pengguna->GetUsername())
-                                ->get()
-                                ->transform(function($row){
-                                    return Creator::buildPengguna(PenggunaEntitas::make(
-                                        $row->id,
-                                        null,
-                                        $row->NPribadi?->nip,
-                                        $row->username,
-                                        $row->password,
-                                        $row->NPribadi?->nama,
-                                        null,
-                                        null,
-                                        null,
-                                        $row->PayrollPegawai?->struktural,
-                                        $row->Pengangkatan?->unit_kerja,
-                                        strtolower($row->level),
-                                        true
-                                    ));
-                                })
-                                ->filter(fn($data)=> $data->AuthenticationSimpeg($pengguna->GetUsername(),$pengguna->GetPassword()) );
+                                ->get();
+                                dd($listPengguna);
+                                // ->transform(function($row){
+                                //     return Creator::buildPengguna(PenggunaEntitas::make(
+                                //         $row->id,
+                                //         null,
+                                //         $row->NPribadi?->nip,
+                                //         $row->username,
+                                //         $row->password,
+                                //         $row->NPribadi?->nama,
+                                //         null,
+                                //         null,
+                                //         null,
+                                //         $row->PayrollPegawai?->struktural,
+                                //         $row->Pengangkatan?->unit_kerja,
+                                //         strtolower($row->level),
+                                //         true
+                                //     ));
+                                // })
+                                // ->filter(fn($data)=> $data->AuthenticationSimpeg($pengguna->GetUsername(),$pengguna->GetPassword()) );
 
         if($listPengguna->count()>1) throw new Exception("akun ".$pengguna->GetUsername()." lebih dari 1");
         if($listPengguna->count()==0) throw new Exception("akun tidak ditemukan");
