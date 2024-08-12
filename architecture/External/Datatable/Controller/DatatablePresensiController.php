722d5c30e3869ba9a0c41599188e6a0451e2dce0
!Github@251423! <studiocode52@gmail.com>
2024-08-12 14:53:06 +0700

    debug

--- architecture/External/Datatable/Controller/DatatablePresensiController.php
+++ architecture/External/Datatable/Controller/DatatablePresensiController.php
@@ -39,8 +39,8 @@ class DatatablePresensiController extends Controller
                     //     return $item->getTanggal()->isEqual(new Date(date('Y-m-d')));
                     // })
                     ->map(function ($item) use($filter){
-                        return match(true){
-                            $filter=="dosen"=>[
+                        return match($filter){
+                            "dosen"=>[
                                 "nama"=>$item->GetDosen()?->GetNama()." - ".$item->GetDosen()?->GetNIDN()." - ".$item->GetDosen()?->GetUnitKerja(),
                                 "tanggal"=>$item->GetTanggal()?->toFormat(FormatDate::LDFY),
                                 "masuk"=>$item->GetAbsenMasuk()?->toFormat(FormatDate::HIS),
@@ -48,7 +48,7 @@ class DatatablePresensiController extends Controller
                                 "catatan_telat"=>$item->GetCatatanTelat(),
                                 "catatan_pulang"=>$item->GetCatatanPulang(),
                             ],
-                            $filter=="pegawai"=>[
+                            "pegawai"=>[
                                 "nama"=>$item->GetPegawai()?->GetNama()." - ".$item->GetPegawai()?->GetNIP()." - ".$item->GetPegawai()?->GetUnit(),
                                 "tanggal"=>$item->GetTanggal()?->toFormat(FormatDate::LDFY),
                                 "masuk"=>$item->GetAbsenMasuk()?->toFormat(FormatDate::HIS),
