b176ddd3c69c19585c80fcef880cb5aed3d25d80
adamf0 <59599463+adamf0@users.noreply.github.com>
2024-08-24 18:26:46 +0700

    debug

--- architecture/External/Datatable/Controller/DatatableSPPDController.php
+++ architecture/External/Datatable/Controller/DatatableSPPDController.php
@@ -41,7 +41,11 @@ class DatatableSPPDController extends Controller
                                     // ($type=="dosen" && !empty($item->GetDosen()) && $item->GetDosen()?->GetNidn()==$nidn) || 
                                     // ($type=="tendik" && !empty($item->GetPegawai()) && $item->GetPegawai()?->GetNip()==$nip);
                                 } else {
-                                    dump($item);
+                                    dump(
+                                        $type=="dosen" && !empty($item->GetDosen()),
+                                        $type=="tendik" && !empty($item->GetPegawai()),
+                                        in_array($item->GetStatus(), ["menunggu verifikasi sdm","terima sdm","tolak sdm"])
+                                    );
                                     return (
                                         ($type=="dosen" && !empty($item->GetDosen())) || 
                                         ($type=="tendik" && !empty($item->GetPegawai()))
