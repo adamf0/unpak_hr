435dc4e943ef590ff50daa7633b0cc1849c418fb
!Github@251423! <studiocode52@gmail.com>
2024-08-27 09:36:31 +0700

    debug

--- architecture/External/Select2/Controller/TestSelect2PresensiController.php
+++ architecture/External/Select2/Controller/TestSelect2PresensiController.php
@@ -26,62 +26,30 @@ class TestSelect2PresensiController extends Controller
 
     public function List(Request $request)
     {
-        $nidn = $request->has('nidn') ? $request->query('nidn') : null;
-        $nip = $request->has('nip') ? $request->query('nip') : null;
-        // $level = $request->has('level')? $request->query('level'):null;
+        try {
+            $nidn = $request->has('nidn') ? $request->query('nidn') : null;
+            $nip = $request->has('nip') ? $request->query('nip') : null;
+            // $level = $request->has('level')? $request->query('level'):null;
 
-        $today = Carbon::today();
-        $dateRange = collect(range(1, 2))->reduce(function ($carry, $item) use ($today) {
-            $carry[] = $today->copy()->subDays($item)->format('Y-m-d');
-            return $carry;
-        }, []);
-
-        $listPresensi = $this->queryBus->ask(new GetAllPresensiInRangeQuery($nidn, $nip, $dateRange));
-
-        $list_izin = $this->queryBus->ask(new GetAllIzinInRangeQuery($nidn, $nip, $dateRange));
-        $listIzin = $list_izin->reduce(function ($list, $item) {
-            $list[] = $item->GetTanggalPengajuan()?->toFormat(FormatDate::Default);
-
-            return $list;
-        }, []);
-
-        $list_cuti = $this->queryBus->ask(new GetAllCutiInRangeQuery($nidn, $nip, $dateRange));
-        $listCuti = $list_cuti->reduce(function ($list, $item) {
-            $start  = $item->GetTanggalMulai()->val();
-            $end    = $item->GetTanggalAkhir()->val();
-            $days   = $end->diffInDays($start);
-
-            $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
-                $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
+            $today = Carbon::today();
+            $dateRange = collect(range(1, 2))->reduce(function ($carry, $item) use ($today) {
+                $carry[] = $today->copy()->subDays($item)->format('Y-m-d');
                 return $carry;
             }, []);
 
-            $list[] = $listDate;
+            $listPresensi = $this->queryBus->ask(new GetAllPresensiInRangeQuery($nidn, $nip, $dateRange));
 
-            return $list;
-        }, []);
+            $list_izin = $this->queryBus->ask(new GetAllIzinInRangeQuery($nidn, $nip, $dateRange));
+            $listIzin = $list_izin->reduce(function ($list, $item) {
+                $list[] = $item->GetTanggalPengajuan()?->toFormat(FormatDate::Default);
 
-        $list_sppd = $this->queryBus->ask(new GetAllSPPDInRangeQuery($nidn, $nip, $dateRange));
-        $listSppd = $list_sppd->reduce(function ($list, $item) {
-            $start  = $item->GetTanggalBerangkat()->val();
-            $end    = $item->GetTanggalKembali()->val();
-            $days   = $end->diffInDays($start);
-
-            $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
-                $carry[] = $item->GetTanggalBerangkat()->val()->addDays($i)->format('Y-m-d');
-                return $carry;
+                return $list;
             }, []);
 
-            $list[] = $listDate;
-
-            return $list;
-        }, []);
-
-        $list_libur = $this->queryBus->ask(new GetAllMasterKalendarInRangeQuery($dateRange));
-        $listLibur = $list_libur->reduce(function ($list, $item) {
-            if (!empty($item->GetTanggalMulai())) {
+            $list_cuti = $this->queryBus->ask(new GetAllCutiInRangeQuery($nidn, $nip, $dateRange));
+            $listCuti = $list_cuti->reduce(function ($list, $item) {
                 $start  = $item->GetTanggalMulai()->val();
-                $end    = empty($item->GetTanggalAkhir()) ? $item->GetTanggalMulai()->val() : $item->GetTanggalAkhir()->val();
+                $end    = $item->GetTanggalAkhir()->val();
                 $days   = $end->diffInDays($start);
 
                 $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
@@ -90,40 +58,76 @@ class TestSelect2PresensiController extends Controller
                 }, []);
 
                 $list[] = $listDate;
-            }
 
-            return $list;
-        }, []);
+                return $list;
+            }, []);
 
-        $listTanggal = array_merge(Arr::flatten($listCuti), Arr::flatten($listSppd), Arr::flatten($listLibur), $listIzin);
-        $listTanggal = array_values(array_unique($listTanggal));
+            $list_sppd = $this->queryBus->ask(new GetAllSPPDInRangeQuery($nidn, $nip, $dateRange));
+            $listSppd = $list_sppd->reduce(function ($list, $item) {
+                $start  = $item->GetTanggalBerangkat()->val();
+                $end    = $item->GetTanggalKembali()->val();
+                $days   = $end->diffInDays($start);
 
-        $rangeTanggal = array_reduce(range(1, 2), function ($carry, $i){
-            $carry[] = Carbon::now()->subDays($i)->format('Y-m-d');
-            return $carry;
-        }, []);
+                $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
+                    $carry[] = $item->GetTanggalBerangkat()->val()->addDays($i)->format('Y-m-d');
+                    return $carry;
+                }, []);
 
-        $list = $listPresensi->filter(function ($item) use ($listTanggal, $rangeTanggal) {
-            return !in_array($item->GetTanggal()->toFormat(FormatDate::Default), $listTanggal) && 
-                    $item->GetAbsenMasuk() instanceof Date && 
-                    in_array($item->GetTanggal()->toFormat(FormatDate::Default),$rangeTanggal);
-        })
-        ->values()
-        ->sortBy('tanggal', SORT_REGULAR, true)
-        ->reduce(function ($list, $item) {
-            $text = $item->GetTanggal()->toFormat(FormatDate::LDFY);
-            if (!is_null($item?->GetAbsenMasuk()) && !is_null($item?->GetAbsenKeluar())) {
-                $text .= " (" . $item?->GetAbsenMasuk()?->toFormat(FormatDate::HIS) . " - " . $item?->GetAbsenKeluar()?->toFormat(FormatDate::HIS) . ")";
-            } else if (!is_null($item?->GetAbsenMasuk()) && is_null($item?->GetAbsenKeluar())) {
-                $text .= " (" . $item?->GetAbsenMasuk()?->toFormat(FormatDate::HIS) . " - saat ini)";
-            }
+                $list[] = $listDate;
 
-            $list[] = [
-                "id" => $item->GetId(),
-                "text" => $text,
-            ];
-            return $list;
-        },[]);
+                return $list;
+            }, []);
+
+            $list_libur = $this->queryBus->ask(new GetAllMasterKalendarInRangeQuery($dateRange));
+            $listLibur = $list_libur->reduce(function ($list, $item) {
+                if (!empty($item->GetTanggalMulai())) {
+                    $start  = $item->GetTanggalMulai()->val();
+                    $end    = empty($item->GetTanggalAkhir()) ? $item->GetTanggalMulai()->val() : $item->GetTanggalAkhir()->val();
+                    $days   = $end->diffInDays($start);
+
+                    $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
+                        $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
+                        return $carry;
+                    }, []);
+
+                    $list[] = $listDate;
+                }
+
+                return $list;
+            }, []);
+
+            $listTanggal = array_merge(Arr::flatten($listCuti), Arr::flatten($listSppd), Arr::flatten($listLibur), $listIzin);
+            $listTanggal = array_values(array_unique($listTanggal));
+
+            $rangeTanggal = array_reduce(range(1, 2), function ($carry, $i){
+                $carry[] = Carbon::now()->subDays($i)->format('Y-m-d');
+                return $carry;
+            }, []);
+
+            $list = $listPresensi->filter(function ($item) use ($listTanggal, $rangeTanggal) {
+                return !in_array($item->GetTanggal()->toFormat(FormatDate::Default), $listTanggal) && 
+                        $item->GetAbsenMasuk() instanceof Date && 
+                        in_array($item->GetTanggal()->toFormat(FormatDate::Default),$rangeTanggal);
+            })
+            ->values()
+            ->sortBy('tanggal', SORT_REGULAR, true)
+            ->reduce(function ($list, $item) {
+                $text = $item->GetTanggal()->toFormat(FormatDate::LDFY);
+                if (!is_null($item?->GetAbsenMasuk()) && !is_null($item?->GetAbsenKeluar())) {
+                    $text .= " (" . $item?->GetAbsenMasuk()?->toFormat(FormatDate::HIS) . " - " . $item?->GetAbsenKeluar()?->toFormat(FormatDate::HIS) . ")";
+                } else if (!is_null($item?->GetAbsenMasuk()) && is_null($item?->GetAbsenKeluar())) {
+                    $text .= " (" . $item?->GetAbsenMasuk()?->toFormat(FormatDate::HIS) . " - saat ini)";
+                }
+
+                $list[] = [
+                    "id" => $item->GetId(),
+                    "text" => $text,
+                ];
+                return $list;
+            },[]);
+        } catch (\Exception $e) {
+            throw $e;
+        }
 
         return response()->json(count($list)>=2? array_slice($list,0,2):$list);
     }
