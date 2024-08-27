c5016a3ad725cf9c6ba8c353ebc66741003132ae
!Github@251423! <studiocode52@gmail.com>
2024-08-27 09:58:52 +0700

    debug

--- architecture/External/Select2/Controller/TestSelect2PresensiController.php
+++ architecture/External/Select2/Controller/TestSelect2PresensiController.php
@@ -40,61 +40,61 @@ class TestSelect2PresensiController extends Controller
             $listPresensi = $this->queryBus->ask(new GetAllPresensiInRangeQuery($nidn, $nip, $dateRange));
 
             $list_izin = $this->queryBus->ask(new GetAllIzinInRangeQuery($nidn, $nip, $dateRange));
-            $listIzin = $list_izin->reduce(function ($list, $item) {
-                $list[] = $item->GetTanggalPengajuan()?->toFormat(FormatDate::Default);
+            // $listIzin = $list_izin->reduce(function ($list, $item) {
+            //     $list[] = $item->GetTanggalPengajuan()?->toFormat(FormatDate::Default);
 
-                return $list;
-            }, []);
+            //     return $list;
+            // }, []);
 
             $list_cuti = $this->queryBus->ask(new GetAllCutiInRangeQuery($nidn, $nip, $dateRange));
-            $listCuti = $list_cuti->reduce(function ($list, $item) {
-                $start  = $item->GetTanggalMulai()->val();
-                $end    = $item->GetTanggalAkhir()->val();
-                $days   = $end->diffInDays($start);
+            // $listCuti = $list_cuti->reduce(function ($list, $item) {
+            //     $start  = $item->GetTanggalMulai()->val();
+            //     $end    = $item->GetTanggalAkhir()->val();
+            //     $days   = $end->diffInDays($start);
 
-                $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
-                    $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
-                    return $carry;
-                }, []);
+            //     $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
+            //         $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
+            //         return $carry;
+            //     }, []);
 
-                $list[] = $listDate;
+            //     $list[] = $listDate;
 
-                return $list;
-            }, []);
+            //     return $list;
+            // }, []);
 
             $list_sppd = $this->queryBus->ask(new GetAllSPPDInRangeQuery($nidn, $nip, $dateRange));
-            $listSppd = $list_sppd->reduce(function ($list, $item) {
-                $start  = $item->GetTanggalBerangkat()->val();
-                $end    = $item->GetTanggalKembali()->val();
-                $days   = $end->diffInDays($start);
+            // $listSppd = $list_sppd->reduce(function ($list, $item) {
+            //     $start  = $item->GetTanggalBerangkat()->val();
+            //     $end    = $item->GetTanggalKembali()->val();
+            //     $days   = $end->diffInDays($start);
 
-                $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
-                    $carry[] = $item->GetTanggalBerangkat()->val()->addDays($i)->format('Y-m-d');
-                    return $carry;
-                }, []);
+            //     $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
+            //         $carry[] = $item->GetTanggalBerangkat()->val()->addDays($i)->format('Y-m-d');
+            //         return $carry;
+            //     }, []);
 
-                $list[] = $listDate;
+            //     $list[] = $listDate;
 
-                return $list;
-            }, []);
+            //     return $list;
+            // }, []);
 
             $list_libur = $this->queryBus->ask(new GetAllMasterKalendarInRangeQuery($dateRange));
-            $listLibur = $list_libur->reduce(function ($list, $item) {
-                if (!empty($item->GetTanggalMulai())) {
-                    $start  = $item->GetTanggalMulai()->val();
-                    $end    = empty($item->GetTanggalAkhir()) ? $item->GetTanggalMulai()->val() : $item->GetTanggalAkhir()->val();
-                    $days   = $end->diffInDays($start);
+            // $listLibur = $list_libur->reduce(function ($list, $item) {
+            //     if (!empty($item->GetTanggalMulai())) {
+            //         $start  = $item->GetTanggalMulai()->val();
+            //         $end    = empty($item->GetTanggalAkhir()) ? $item->GetTanggalMulai()->val() : $item->GetTanggalAkhir()->val();
+            //         $days   = $end->diffInDays($start);
 
-                    $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
-                        $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
-                        return $carry;
-                    }, []);
+            //         $listDate = array_reduce(range(0, $days + 1), function ($carry, $i) use ($item) {
+            //             $carry[] = $item->GetTanggalMulai()->val()->addDays($i)->format('Y-m-d');
+            //             return $carry;
+            //         }, []);
 
-                    $list[] = $listDate;
-                }
+            //         $list[] = $listDate;
+            //     }
 
-                return $list;
-            }, []);
+            //     return $list;
+            // }, []);
 
             // $listTanggal = array_merge(Arr::flatten($listCuti), Arr::flatten($listSppd), Arr::flatten($listLibur), $listIzin);
             // $listTanggal = array_values(array_unique($listTanggal));
@@ -131,10 +131,10 @@ class TestSelect2PresensiController extends Controller
 
         dd(
             $listPresensi,
-            $listCuti,
-            $listIzin,
-            $listSppd,
-            $listLibur,
+            $list_cuti,
+            $list_izin,
+            $list_sppd,
+            $list_libur,
         );
         // return response()->json(count($list)>=2? array_slice($list,0,2):$list);
     }
