d4e910dc9cbc00ac3e058f40f89b1bb385fa01c6
!Github@251423! <studiocode52@gmail.com>
2024-09-02 10:31:17 +0700

    fix bug

--- architecture/External/Datatable/Controller/DatatableIzinController.php
+++ architecture/External/Datatable/Controller/DatatableIzinController.php
@@ -28,7 +28,6 @@ class DatatableIzinController extends Controller
         // $q->SetOffset($request->get('start')??null)->SetLimit($request->get('length')??null);
         
         $listIzin = $this->queryBus->ask($q);
-        dump($listIzin);
         $listIzin = $listIzin->filter(function($item) use($level,$type,$verifikasi,$nidn,$nip){
                         $rule1 = (
                             (!empty($item->GetVerifikasi()?->GetNidn()) && $item->GetVerifikasi()?->GetNidn()==$nidn) ||
