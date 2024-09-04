91aeeb441b0a4d4fb13f4110168ab8c118c18c90
!Github@251423! <studiocode52@gmail.com>
2024-09-04 16:25:54 +0700

    debug

--- architecture/External/Persistance/Queries/Pegawai/GetAllPegawaiV2QueryHandler.php
+++ architecture/External/Persistance/Queries/Pegawai/GetAllPegawaiV2QueryHandler.php
@@ -16,7 +16,7 @@ class GetAllPegawaiV2QueryHandler extends Query
     public function handle(GetAllPegawaiV2Query $query)
     {
         $datas = PayrollPegawai::select('e_pribadi.nidn','payroll_m_pegawai.nama','payroll_m_pegawai.struktural')
-                    ->join('e_pribadi', 'payroll_m_pegawai.nip = e_pribadi.nip')
+                    ->join('e_pribadi', 'payroll_m_pegawai.nip', '=', 'e_pribadi.nip')
                     ->whereRaw('LENGTH(nip)>=3');
         if($query->GetStruktural()=="verifikator"){
             $datas = $datas->where('struktural','like','%Wakil Rektor Bid SDM dan Keuangan%')->orWhere('struktural','like','%Wakil Dekan 2%');
