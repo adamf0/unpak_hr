f29b716fa3c9f16a44f40f347e57297fd2cea61e
!Github@251423! <studiocode52@gmail.com>
2024-09-20 13:31:12 +0700

    debug

--- architecture/External/Web/Controller/TesController.php
+++ architecture/External/Web/Controller/TesController.php
@@ -54,33 +54,33 @@ class TesController extends Controller
         ->get();
 
         // Map the result to match the desired format
-        $mappedData = $presensiData->reduce(function($carry,$item) {
-            $carry[] = [
-                'nip_pegawai' => $item?->Pribadi?->nip ?? null,
-                'nama_pegawai' => $item?->Pribadi?->nama ?? null,
-                'nip_dosen' => $item?->Dosen?->nip ?? null,
-                'nidn_dosen' => $item?->Dosen?->nidn ?? null,
-                'nama_dosen' => $item?->Dosen?->nama_dosen ?? null,
-                'kode_fakultas' => $item?->Dosen?->Fakultas?->kode_fakultas ?? null,
-                'nama_fakultas' => $item?->Dosen?->Fakultas?->nama_fakultas ?? null,
-                'kode_prodi' => $item?->Dosen?->Prodi?->kode_prodi ?? null,
-                'nama_prodi' => $item?->Dosen?->Prodi?->nama_prodi ?? null,
-                'unit_kerja' => $item?->Pribadi?->Pengangkatan?->unit_kerja ?? null,
-                'status' => $item?->Pribadi?->Pengangkatan?->status,
-                'id' => $item?->id,
-                'tanggal' => $item?->tanggal,
-                'absen_masuk' => $item?->absen_masuk,
-                'absen_keluar' => $item?->absen_keluar,
-                'catatan_telat' => $item?->catatan_telat,
-                'catatan_pulang' => $item?->catatan_pulang,
-                'otomatis_keluar' => $item?->otomatis_keluar,
-                'created_at' => $item?->created_at,
-                'updated_at' => $item?->updated_at,
-            ];
+        // $mappedData = $presensiData->reduce(function($carry,$item) {
+        //     $carry[] = [
+        //         'nip_pegawai' => $item?->Pribadi?->nip ?? null,
+        //         'nama_pegawai' => $item?->Pribadi?->nama ?? null,
+        //         'nip_dosen' => $item?->Dosen?->nip ?? null,
+        //         'nidn_dosen' => $item?->Dosen?->nidn ?? null,
+        //         'nama_dosen' => $item?->Dosen?->nama_dosen ?? null,
+        //         'kode_fakultas' => $item?->Dosen?->Fakultas?->kode_fakultas ?? null,
+        //         'nama_fakultas' => $item?->Dosen?->Fakultas?->nama_fakultas ?? null,
+        //         'kode_prodi' => $item?->Dosen?->Prodi?->kode_prodi ?? null,
+        //         'nama_prodi' => $item?->Dosen?->Prodi?->nama_prodi ?? null,
+        //         'unit_kerja' => $item?->Pribadi?->Pengangkatan?->unit_kerja ?? null,
+        //         'status' => $item?->Pribadi?->Pengangkatan?->status,
+        //         'id' => $item?->id,
+        //         'tanggal' => $item?->tanggal,
+        //         'absen_masuk' => $item?->absen_masuk,
+        //         'absen_keluar' => $item?->absen_keluar,
+        //         'catatan_telat' => $item?->catatan_telat,
+        //         'catatan_pulang' => $item?->catatan_pulang,
+        //         'otomatis_keluar' => $item?->otomatis_keluar,
+        //         'created_at' => $item?->created_at,
+        //         'updated_at' => $item?->updated_at,
+        //     ];
 
-            return $carry;
-        });
+        //     return $carry;
+        // });
 
-        return response()->json($mappedData);
+        return response()->json($presensiData);
     }
 }
