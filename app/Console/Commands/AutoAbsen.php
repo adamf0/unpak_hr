b04060e91b0c49d29973858354a866215e58381d
!Github@251423! <studiocode52@gmail.com>
2024-10-02 08:20:47 +0700

    add auto absen command

---
+++ app/Console/Commands/AutoAbsen.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Architecture\External\Persistance\ORM\Absensi;
+use Architecture\External\Persistance\ORM\Dosen;
+use Architecture\External\Persistance\ORM\NPribadi;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+
+class AutoAbsen extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'auto:absen';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'pembuatan auto absen';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try {
+            DB::beginTransaction();
+                Absensi::whereIn('nip',[4102206194,2110816436,4102302214,2111018464,2110816439,2110718457,207241098534,2110816441,2110121489])
+                        ->update(['absen_masuk' => date('Y-m-d 06:00:00')]);
+            DB::commit();
+            echo "success create absent"; 
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+}
