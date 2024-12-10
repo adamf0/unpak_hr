d9de5e770dba2eb509e0f4f7a0224f755668a44c
!Github@251423! <studiocode52@gmail.com>
2024-12-10 08:28:09 +0700

    update

--- resources/views/authorization.blade.php
+++ resources/views/authorization.blade.php
@@ -83,17 +83,13 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
   @stack('scripts')
-  <script>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X338084FQ3"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XYJ8LJNYGV"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-X338084FQ3');
-</script>
-
-
+  gtag('config', 'G-XYJ8LJNYGV');
 </script>
 </body>
 
--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -387,13 +387,13 @@
       });
     });
   </script>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-X338084FQ3"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XYJ8LJNYGV"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-X338084FQ3');
+  gtag('config', 'G-XYJ8LJNYGV');
 </script>
 
 </body>
