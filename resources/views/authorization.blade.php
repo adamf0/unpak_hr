1fcc1c024f4e91a15c5edc6874a4cc1565854c8a
!Github@251423! <studiocode52@gmail.com>
2024-12-13 15:49:03 +0700

    update

--- resources/views/authorization.blade.php
+++ resources/views/authorization.blade.php
@@ -73,10 +73,10 @@
                               </div>
                             </div>
                             <div class="col-12 d-grid gap-4">
-                              <button class="btn btn-dark btn-lg" type="submit">Login</button>
+                              <button class="btn btn-dark" type="submit">Login</button>
                               <div class="col-12 d-grid gap-2">
-                                <a href="https://unpak.link/hrportal_app" target="_blank" class="btn btn-success btn-md col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><i class="bi bi-android2"></i> Versi Android</a>
-                                <a href="{{ Utility::loadAsset('USER GUIDE DOSEN & TENDIK NON STRUKTURAL (26-08-2024).pdf')}}" target="_blank" class="btn btn-primary btn-md col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><i class="bi bi-journal-album"></i> Manual Guide</a>
+                                <a href="https://unpak.link/hrportal_app" target="_blank" class="btn btn-success"><i class="bi bi-android2"></i> Versi Android</a>
+                                <a href="{{ Utility::loadAsset('USER GUIDE DOSEN & TENDIK NON STRUKTURAL (26-08-2024).pdf')}}" target="_blank" class="btn btn-primary"><i class="bi bi-journal-album"></i> Manual Guide</a>
                               </div>
                             </div>
                           </div>
